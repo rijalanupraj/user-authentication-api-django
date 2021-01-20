3a144344979d375ebb94d49be40a90f556e9315e
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-01-20 19:10:00 +0545

    Add Validation of username in serializer

--- user/serializers.py
+++ user/serializers.py
@@ -4,13 +4,35 @@ from rest_framework import serializers
 # Internal Import
 from . import models
 
+
 class UserRegistrationSerializer(serializers.ModelSerializer):
     """
     Serializer to Register New User Using CustomUser Model
     """
-    password = serializers.CharField(max_length=60, min_length=6, write_only=True)
+    password = serializers.CharField(
+        max_length=60, min_length=6, write_only=True)
     username = serializers.CharField(max_length=20, min_length=4)
 
     class Meta:
         model = models.CustomUser
-        fields = ('id','username','email','password')
+        fields = ('id', 'username', 'email', 'password')
+
+    def validate(self, attrs):
+        """
+        Validating the Data provided by the user by POST request during register
+        """
+        username = attrs.get('username', '')  # Removing spaces from username
+
+        # Username can only have number or alphabets
+        if not username.isalnum():
+            raise serializers.ValidationError({
+                'username': 'Username should be of alphabetical characters and numbers only'
+            })
+
+        # Username should cannot start with numbers
+        elif username[0].isdigit():
+            raise serializers.ValidationError({
+                'username': 'The username should start with alphabetical Characters'
+            })
+
+        return attrs
