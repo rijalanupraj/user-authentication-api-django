f7b8ba411a61c828d66712d010072e0812d9c081
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-01-20 19:18:32 +0545

    Update User Registration generic api view

--- user/views.py
+++ user/views.py
@@ -1,8 +1,10 @@
 # External Import
-from rest_framework import generics
+from rest_framework import generics, response, status
 
 # Internal Import
 from . import serializers
+from . import models
+
 
 class UserRegistrationView(generics.GenericAPIView):
     """
@@ -11,7 +13,24 @@ class UserRegistrationView(generics.GenericAPIView):
 
     serializer_class = serializers.UserRegistrationSerializer
 
-    def post(self,request):
+    def post(self, request):
         """
         Post Request for User Registration
         """
+        currentUser = request.data  # Storing the POST data in a variable
+
+        serializer = self.serializer_class(data=currentUser)
+
+        # Working on the data if only the data provided of the user is valid
+        # Serializer checks the validity of the USER Post Request Data
+        if serializer.is_valid():
+            serializer.save()  # Saving the data to database
+            currentUserData = serializer.data
+
+            # Returning custom response while user is Created & Saved in Database
+            return response.Response({
+                'status': status.HTTP_201_CREATED,
+                'message': 'Accont Created',
+                'response': currentUserData
+            })
+
